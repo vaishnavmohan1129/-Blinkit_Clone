3980390d8bbbb7a2298d5bb3f74358b6c2f1d562
vaishnavmohan1129 <vaishnavmohan1129@gmail.com>
2025-02-14 22:10:01 +0530

    first commit

---
+++ server/models/category.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema({
+    name :{
+        type : String,
+        default : ""
+    },
+    image : {
+        type : String,
+        default : ""
+    }
+},{
+    timestamps : true
+})
+
+const categoryModel = mongoose.model('category',categorySchema)
+export default categoryModel
---
+++ server/utils/verifyEmailTemplate.js
@@ -0,0 +1,19 @@
+const verifyEmailTemplate = ({ name, url }) => {
+    return `
+        <p>Dear ${name},</p>
+        <p>Thank you for registering at Binkeyit.</p>
+        <a href="${url}" style="
+            color: black;
+            background: orange;
+            margin-top: 10px;
+            padding: 10px 20px;
+            display: inline-block;
+            text-decoration: none;
+            font-weight: bold;
+        ">
+            Verify Email
+        </a>
+    `;
+};
+
+export default verifyEmailTemplate;
